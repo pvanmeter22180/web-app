d453b40e3155ae46a88e578b98f3d65ed108c415
pvanmeter22180 <50497566+pvanmeter22180@users.noreply.github.com>
2021-03-11 12:48:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 const path = require("path");
 const { createServer } = require("http");
 const { auth, requiresAuth } = require("express-openid-connect");
+const axios = require("axios").default;
 
 const {
   checkUrl,
@@ -46,18 +47,6 @@ app.use(
  })
 );
 
-const expenses = [
-  {
-    date: new Date(),
-    description: "Pizza for a Coding Dojo session.",
-    value: 102,
-  },
-  {
-    date: new Date(),
-    description: "Coffee for a Coding Dojo session.",
-    value: 42,
-  },
-];
 
 app.get("/", async (req, res) => {
   res.render("home", {
@@ -67,7 +56,19 @@ app.get("/", async (req, res) => {
   });
 });
 
-// 👇 add requiresAuth middlware to these private routes  👇
+app.get("/", async (req, res) => {
+ try {
+   const summary = await axios.get(`${API_URL}/total`);
+   res.render("home", {
+     user: req.oidc && req.oidc.user,
+     total: summary.data.total,
+     count: summary.data.count,
+   });
+ } catch (err) {
+   next(err);
+ }
+});
+
 
 app.get("/user", requiresAuth(), async (req, res) => {
   res.render("user", {
@@ -79,11 +80,16 @@ app.get("/user", requiresAuth(), async (req, res) => {
 });
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
-  res.render("expenses", {
-    user: req.oidc && req.oidc.user,
-    expenses,
-  });
-});
+   try {
+     const expenses = await axios.get(`${API_URL}/reports`);
+     res.render("expenses", {
+       user: req.oidc && req.oidc.user,
+       expenses: expenses.data,
+     });
+   } catch (err) {
+     next(err);
+   }
+ });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
